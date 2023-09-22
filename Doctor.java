790020a8eb79c033ff98aaf5a7e92006314da766
Justin D <isaac_justin@hotmail.com>
2023-09-22 11:54:53 -0600

    ADD: UIMenu with static methods & final

--- Doctor.java
+++ Doctor.java
@@ -1,15 +1,22 @@
 public class Doctor {
-    int id;
+    static int id; // el static nos permite hacer el autoincremental de la variable sin importar la
+                   // instanciacion
+    // int ID; una variable en mayusculas indica una constante.
     String name;
     String speciality;
 
     // constructor
     Doctor() {
         System.out.println("Esto se manda a imprimir desde el constructor");
+        id++;
     }
 
     // methods
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import static ui.UIMenu.*; // asi hacemos ei import de los metodo estaticos de la clase
+
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Papucho";
-
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctor2 = new Doctor();
+        myDoctor2.showId();
+
+        showMenu();
     }
 }
