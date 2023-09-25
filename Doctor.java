a17bd4d447a916278311c27cda85d09c15d0e222
Justin D <isaac_justin@hotmail.com>
2023-09-25 10:43:47 -0600

    ADD: nested classes

--- Doctor.java
+++ Doctor.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     // atributos
     static int id; // el static nos permite hacer el autoincremental de la variable sin importar la
                    // instanciacion
     // int ID; una variable en mayusculas indica una constante.
-    String name;
-    String speciality;
+    private String name;
+    private String speciality;
+    private String email;
 
     // constructor
     Doctor() {
@@ -26,5 +30,43 @@ public class Doctor {
 
     public void showId() {
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    // clase anidada
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
 import static ui.UIMenu.*; // asi hacemos ei import de los metodo estaticos de la clase
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         Doctor myDoctor = new Doctor();
-        myDoctor.name = "Papucho";
-        myDoctor.showName();
-        myDoctor.showId();
+        // myDoctor.name = "Papucho";
+        // myDoctor.showName();
+        // myDoctor.showId();
 
-        Doctor myDoctor2 = new Doctor();
-        myDoctor2.showId();
+        // Doctor myDoctor2 = new Doctor();
+        // myDoctor2.showId();
 
         // showMenu();
+
+        // nested classes
+        myDoctor.addAvailableAppointment(new Date(), "3 pm");
+        myDoctor.addAvailableAppointment(new Date(), "4 pm");
+        myDoctor.addAvailableAppointment(new Date(), "5 pm");
+        myDoctor.addAvailableAppointment(new Date(), "6 pm");
+
+        for (Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
 
         Patient patient = new Patient("papucho", "papucho@email.com");
         patient.setWeight(59.7);
